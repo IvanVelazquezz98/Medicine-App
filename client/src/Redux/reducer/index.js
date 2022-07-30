1511ad27bfd3726c2559206bb255b3e1da2d1d57
IvanVelazquezz98 <Velazquez.ivan.e98@gmail.com>
2022-07-30 15:15:15 -0300

    App ready to Work

---
+++ client/src/Redux/reducer/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+
+}
+
+export default function rootReducer(state= initialState , action){
+    switch(action.type){
+        //case:
+
+        default: return state
+    }
+}
