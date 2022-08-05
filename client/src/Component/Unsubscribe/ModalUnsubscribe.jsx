efe5c54a63396135f2db882d1aa50f372bd49649
neleon10 <cvn45@hotmail.com>
2022-08-04 21:29:50 -0300

    emailsJs_configured

--- client/src/Component/Unsubscribe/ModalUnsubscribe.jsx
+++ client/src/Component/Unsubscribe/ModalUnsubscribe.jsx
@@ -2,12 +2,23 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import emailjs from '@emailjs/browser';
 
 function ModalUnsubscribe() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  //mail aqui
+  function sendEmail (e){
+    e.preventDefault()
+   
+    emailjs.sendForm('service_5qp9enm','template_cktuhyc',e.target,'jeekxUefyAsuBBz5j')
+    .then(res => console.log(res))
+    .catch(e=> console.log(e))
+
+  }
 
   return (
     <>
@@ -16,21 +27,26 @@ function ModalUnsubscribe() {
       </Button>
 
       <Modal show={show} onHide={handleClose}>
+        
         <Modal.Header closeButton>
           <Modal.Title>Formulario de baja</Modal.Title>
         </Modal.Header>
+
         <Modal.Body>
-          <Form>
+
+          <Form onSubmit={(e) => sendEmail(e)}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Email </Form.Label>
               <Form.Control
                 type="email"
+                name="email"
                 placeholder="name@example.com"
                 autoFocus
               />
               <Form.Label>Nombre </Form.Label>
               <Form.Control
                 type="text"
+                name="name"
                 placeholder="Tu nombre aquí..."
                 autoFocus
               />
@@ -40,18 +56,18 @@ function ModalUnsubscribe() {
               controlId="exampleForm.ControlTextarea1"
             >
               <Form.Label>Contanos el por qué de tu baja.</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control as="textarea" rows={3}  name="message"/>
             </Form.Group>
+            <Button variant="secondary" onClick={handleClose}>
+              Cerrar
+            </Button>
+            <Button variant="primary" onClick={handleClose} type="submit">
+              Enviar
+            </Button>
           </Form>
+
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cerrar
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Enviar
-          </Button>
-        </Modal.Footer>
+        
       </Modal>
     </>
   );
