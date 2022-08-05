2f2fef3026d294cdc5bb02f604dfd37aa48ae018
gabdala17 <gisella.abdala@gmail.com>
2022-08-05 12:30:45 -0300

    Create all Filters and Orders

---
+++ client/src/Component/FilterAndOrder/FilterByServiceType.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { filterTypeService } from '../../Redux-actions'
+
+function FilterByServiceType() {
+    let dispatch= useDispatch()
+    function handleFilter(e){
+        dispatch(filterTypeService(e.target.value))
+    }
+    
+  return (
+    <select name='select' onChange={handleFilter}> 
+        <option value='selected' hidden > Filtrar por tipo de servicio: </option>
+        <option value={'Presencial'}> Presencial </option>
+        <option value={'Virtual'}> Virtual </option>
+        <option value={'A domicilio'}> A domicilio </option>
+      </select>
+  )
+}
+
+export default FilterByServiceType
