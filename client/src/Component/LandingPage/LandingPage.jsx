18d506e0aa3ebf64540abf609f85ce36f238fcda
neleon10 <cvn45@hotmail.com>
2022-08-06 19:39:25 -0300

    footer created

--- client/src/Component/LandingPage/LandingPage.jsx
+++ client/src/Component/LandingPage/LandingPage.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Footer from '../Footer/Footer';
 
 export default function LandingPage() {
 
     
     return (
       <div>
-        
-       
           <h1 /* className={style.h1} */>LANDING PAGE , VA A ESTAR LA PRIEMRA PAGINA DEL WIX</h1>
           <Link  to='/home'><button >clias mck para ver medicos</button></Link>
-        
+          <Footer/>
       </div>
     )}
