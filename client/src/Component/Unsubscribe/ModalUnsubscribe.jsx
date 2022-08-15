68ae3774e6e40cd09be0e2b5ed22a5c354d11edd
neleon10 <cvn45@hotmail.com>
2022-08-15 17:19:48 -0300

    commit to PR-update

--- client/src/Component/Unsubscribe/ModalUnsubscribe.jsx
+++ client/src/Component/Unsubscribe/ModalUnsubscribe.jsx
@@ -1,51 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import FormLabel from "react-bootstrap/esm/FormLabel";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
-import './ModalUnsubscribe.css'
+import "./ModalUnsubscribe.css";
 import { Link } from "react-router-dom";
-import {deleteUserByID,getAds} from '../../Redux-actions/index'
+import { deleteUserByID } from "../../Redux-actions/index";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function ModalUnsubscribe(props) {
-  const [show, setShow] = useState(false);
+  useEffect(() => {
+
+    //get the uid from firebase
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log("este es el uid del que esta logueado", uid);
+      } else {
+        console.log("El usuario se deslogueó.");
+      }
+    });
+  }, []);
+
   
-  const dispatch = useDispatch()
- 
+
+  const [show, setShow] = useState(false);
+
+  const dispatch = useDispatch();
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  function handleSubmit (e){
-    e.preventDefault()
-    dispatch (deleteUserByID(props.user.id))
-    alert ('Realizar la lógica ---> ir a la action')//dar de firebase y redirigir a home
+  function handleSubmit(e) {
+    e.preventDefault();
+     dispatch(deleteUserByID(props.user.id));
+      
   }
+  
 
   return (
     <>
-      <Button onClick={handleShow} size="sm" className="bajaButton" >¿Darse de baja?</Button>
+      <Button onClick={handleShow} size="sm" className="bajaButton">
+        ¿Darse de baja?
+      </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton className="headerBaja">
-          <Modal.Title className="modalTitleBaja">¿Seguro quieres la baja?</Modal.Title>
+          <Modal.Title className="modalTitleBaja">
+            ¿Seguro quieres la baja?
+          </Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <FormLabel className="formLabelBaja">
-                Recuerde que su desvinculación de la aplicación tendrá consecuencias
-                directas sobre la visualización del contenido como así también
-                su historial médico y sus citas reservadas. Cualquier Consulta
-                no dude en comunicarse con el área de <Link to='/'>soporte</Link> al pie de página.
+                Recuerde que su desvinculación de la aplicación tendrá
+                consecuencias directas sobre la visualización del contenido como
+                así también su historial médico y sus citas reservadas.
+                Cualquier Consulta no dude en comunicarse con el área de{" "}
+                <Link to="/">soporte</Link> al pie de página.
               </FormLabel>
             </Form.Group>
 
             <Button variant="secondary" onClick={handleClose}>
               No
             </Button>
-            <Button variant="danger" onClick={handleClose} type="submit" className="yesButtonBaja" >
+            <Button
+              variant="danger"
+              onClick={handleClose}
+              type="submit"
+              className="yesButtonBaja"
+            >
               Darme de baja
             </Button>
           </Form>
