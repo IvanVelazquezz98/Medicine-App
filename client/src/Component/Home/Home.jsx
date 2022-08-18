814fdb483c50f2b464d2bc44f685ff076fc63cf0
neleon10 <cvn45@hotmail.com>
2022-08-17 21:31:22 -0300

    image component rendered in Home Header deleted

--- client/src/Component/Home/Home.jsx
+++ client/src/Component/Home/Home.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Footer from '../Footer/Footer';
-import Navbar from '../Navbar/Navbar'
-import Header from './Header'
-import ModalPayment from './ModalPayment'
-
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+import Header from "./Header";
+import Image from './Image'
+import './Home.css'
+import ModalPayment from "./ModalPayment";
 
 export default function Home() {
-
- 
-    return (
-      <>
-        <Navbar />
-         {/* <Header /> */}
-         
-
-          <Footer/>
-      </>
-    )}
+  return (
+    <div className="HomeMainContainer">
+      <Navbar />
+        <Header />
+        <Image />
+      <Footer />
+    </div>
+  );
+}
--- client/src/Component/Home/Image.jsx
+++ client/src/Component/Home/Image.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import fondo from '../../assets/fondo.png'
-
+import React from "react";
+import fondo from "../../assets/fondo.png";
+import "./Image.css";
 
 function Image() {
   return (
-    <div className="footer-container">
-        <div className="footer-parent">
-            <img src={fondo} alt='No hay conexión a internet ' className='Im'/>
-            </div>
-     
-   </div>
-  )
+    <div className="ImageTopFooter">
+      <img src={fondo} alt="No hay conexión a internet " />
+    </div>
+  );
 }
 
-export default Image
+export default Image;
